e01f0ac28ba28863ece3b47d7273d5bec3685504
lannydev <e.cristina00@gmai.com>
2022-10-18 23:07:39 -0300

    Entendendo a tabela verdade.

--- clinica/src/Fundamentos/operadores/OperadoresLogicos.java
+++ clinica/src/Fundamentos/operadores/OperadoresLogicos.java
@@ -1,2 +1,47 @@
-package Fundamentos;public class OperadoresLogicos {
+package Fundamentos.operadores;
+
+public class OperadoresLogicos {
+
+    public static void main(String[] args) {
+
+        /*
+        && - condição e
+        || - condição ou
+        ^ - ou exclusivo
+        ! - negação
+         */
+
+        boolean condicao1 = true;
+        boolean condicao2 = 2 > 5;
+
+        System.out.println(condicao1 && condicao2);
+        System.out.println(condicao1 || condicao2);
+        System.out.println(condicao1 ^ condicao2);
+        System.out.println(!condicao1);
+        System.out.println(!condicao2);
+
+        System.out.println("\n\n");
+
+        // tabela verdade E
+        System.out.println(true && true); // verdadeiro
+        System.out.println(true && false); // falso
+        System.out.println(false && true); // false
+        System.out.println(false && false); // false
+
+        // tabela verdade OU
+        System.out.println(true || true); // verdadeiro
+        System.out.println(true || false); // verdadeiro
+        System.out.println(false || true); // verdadeiro
+        System.out.println(false || false); // false
+
+
+        // tabela verdade OU exclusivo
+        System.out.println(true ^ true); // falso
+        System.out.println(true ^ false); // verdadeiro
+        System.out.println(false ^ true); // verdadeiro
+        System.out.println(false ^ false); // false
+
+
+    }
+
 }
